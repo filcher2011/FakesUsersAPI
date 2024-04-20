07cf7b1924254e56daf743eef721937c8a6c96c9
filcher <146027639+filcher2011@users.noreply.github.com>
2024-04-20 16:59:50 +0300

    Add files via upload

--- API/views.py
+++ API/views.py
@@ -9,28 +9,35 @@ def main(request):
     return render(request, 'index.html')
 
 @api_view(['GET'])
-def fake_name(request, lang, gender):
-    fu = Fu(lang)
+def fake_name(request):
     if request.method == 'GET':
+        lang = str(request.query_params.get('lang'))
+        gender = str(request.query_params.get('gender'))
+        fu = Fu(lang)
         name = fu.fake_name(gender)  # используйте значение переменной gender
         return Response(name)
 
 @api_view(['GET'])
-def fake_lastname(request, lang, gender):
-    fu = Fu(lang)
+def fake_lastname(request):  
     if request.method == 'GET':
+        lang = str(request.query_params.get('lang'))
+        gender = str(request.query_params.get('gender'))
+        fu = Fu(lang)
         name = fu.fake_lastname(gender)  # используйте значение переменной gender
         return Response(name)
 
 @api_view(['GET'])
-def fake_number(request, lang):
-    fu = Fu(lang)
+def fake_number(request):
     if request.method == 'GET':
+        lang = str(request.query_params.get('lang'))
+        fu = Fu(lang)
         name = fu.fake_number()  # используйте значение переменной gender
         return Response(name)
 
 @api_view(['GET'])
-def fake_addres(request, lang, full):
+def fake_addres(request):
+    lang = str(request.query_params.get('lang'))
+    full = str(request.query_params.get('full'))
     fu = Fu(lang)
     if request.method == 'GET':
         if full == 'True' or full == 'true':
@@ -41,71 +48,81 @@ def fake_addres(request, lang, full):
             return Response(name)
 
 @api_view(['GET'])
-def fake_email(request, lang):
+def fake_email(request):
     if request.method == 'GET':
+        lang = str(request.query_params.get('lang'))
         fu = Fu(lang)
         name = fu.fake_email()  # используйте значение переменной gender
         return Response(name)
 
 @api_view(['GET'])
-def fake_pass(request, lang):
+def fake_pass(request):
     if request.method == 'GET':
+        lang = str(request.query_params.get('lang'))
         fu = Fu(lang)
         name = fu.fake_pass()  # используйте значение переменной gender
         return Response(name)
 
 @api_view(['GET'])
-def fake_dob(request, lang):
+def fake_dob(request):
     if request.method == 'GET':
+        lang = str(request.query_params.get('lang'))
         fu = Fu(lang)
         name = fu.fake_dob()  # используйте значение переменной gender
         return Response(name)
 
 @api_view(['GET'])
-def fake_city(request, lang):
+def fake_city(request):
     if request.method == 'GET':
+        lang = str(request.query_params.get('lang'))
         fu = Fu(lang)
         name = fu.fake_city()  # используйте значение переменной gender
         return Response(name)
 
 @api_view(['GET'])
-def fake_banknumber(request, lang):
+def fake_banknumber(request):
     if request.method == 'GET':
+        lang = str(request.query_params.get('lang'))
         fu = Fu(lang)
         name = fu.fake_banknumber()  # используйте значение переменной gender
         return Response(name)
 
 @api_view(['GET'])
-def fake_login(request, lang):
+def fake_login(request):
     if request.method == 'GET':
+        lang = str(request.query_params.get('lang'))
         fu = Fu(lang)
         name = fu.fake_login()  # используйте значение переменной gender
         return Response(name)
 
 @api_view(['GET'])
-def fake_link(request, lang):
+def fake_link(request):
     if request.method == 'GET':
+        lang = str(request.query_params.get('lang'))
         fu = Fu(lang)
         name = fu.fake_link()  # используйте значение переменной gender
         return Response(name)
 
 @api_view(['GET'])
-def fake_autonum(request, lang):
+def fake_autonum(request):
     if request.method == 'GET':
+        lang = str(request.query_params.get('lang'))
         fu = Fu(lang)
         name = fu.fake_autonum()  # используйте значение переменной gender
         return Response(name)
 
 @api_view(['GET'])
-def fake_text(request, lang):
+def fake_text(request):
     if request.method == 'GET':
+        lang = str(request.query_params.get('lang'))
         fu = Fu(lang)
         name = fu.fake_text()  # используйте значение переменной gender
         return Response(name)
 
 @api_view(['GET'])
-def fake_job(request, lang):
+def fake_job(request):
     if request.method == 'GET':
+        lang = str(request.query_params.get('lang'))
         fu = Fu(lang)
         name = fu.fake_job()  # используйте значение переменной gender
         return Response(name)
